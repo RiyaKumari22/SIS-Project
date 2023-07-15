2d5860601ad5730d3ca61dc4139c60b2f57e5ef0
Riya Kumari <riya.kumari.4rmk@gmail.com>
2023-07-15 14:17:47 +0530

    gallery updated

--- server.js
+++ server.js
@@ -8,20 +8,36 @@ app.set("view engine","ejs")
 app.use(express.static(__dirname + '/public'));
 // app.st(express.static(staticPath))
 app.get('/',(req,res)=>{
-    res.render('index')
+    res.render('index',{"name":"Home"})
+   
+
+
 })
 app.get('/contact',(req,res)=>{
-    res.render('contact')
+    res.render('contact',{"name":"CONTACT US"})
 })
 app.get('/about',(req,res)=>{
-    res.render('about')
+    res.render('about',{'name':'ABOUT US'})
 })
 app.get('/gallery',(req,res)=>{
-    res.render('gallery')
+    res.render('gallery',{'name':'OUR GALLERY'})
 })
-app.get('/header',(req,res)=>{
-    res.render('header')
+app.get('/activity',(req,res)=>{
+    res.render('activity',{'name':'ACTIVITIES'})
 })
+app.get('/bootcamp',(req,res)=>{
+    res.render('bootcamp',{'name':'BOOTCAMPS'})
+})
+app.get('/infrastructure',(req,res)=>{
+    res.render('infrastructure',{'name':'INFRASTRUCTURE'})
+})
+app.get('/placement',(req,res)=>{
+    res.render('placement',{'name':'PLACED STUDENTS'})
+})
+// app.get('/header',(req,res)=>{
+//     res.render('header')
+// })
+
 // app.get('/footer',(req,res)=>{
 //     res.render('footer')
 // })
